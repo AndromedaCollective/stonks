1047fcc8cceea849b3be7f0d366135e4489b5c33
Celine Seghbossian <celineblaine@icloud.com>
2021-08-23 15:21:48 -0700

    updated queries formatting

--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -1,19 +1,19 @@
 CREATE TABLE users (
   user_id INT,
-  username VARCHAR[32],
-  email VARCHAR[64],
+  username VARCHAR(32),
+  email VARCHAR(64),
   PRIMARY KEY (user_id)
 );
 
 CREATE TABLE stocks (
-  ticker VARCHAR[6],
-  stock_name VARCHAR[100],
+  ticker VARCHAR(6),
+  stock_name VARCHAR(100),
   PRIMARY KEY (ticker)
 );
 
 CREATE TABLE investments (
   user_id INT,
-  ticker VARCHAR[6],
+  ticker VARCHAR(6),
   num_shares INT,
   PRIMARY KEY (user_id, ticker),
   FOREIGN KEY (user_id) REFERENCES users(user_id),
@@ -23,17 +23,17 @@ CREATE TABLE investments (
 CREATE TABLE transactions (
   trans_id INT,
   user_id INT,
-  ticker VARCHAR[6],
+  ticker VARCHAR(6),
   num_shares INT,
   date DATE,
   buy_sell BOOLEAN,
   PRIMARY KEY (trans_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (ticker) REFERENCES stocks(ticker)
+  FOREIGN KEY (ticker) REFERENCES stocks (ticker)
 );
 
 CREATE TABLE prices (
-  ticker VARCHAR[6],
+  ticker VARCHAR(6),
   date DATE,
   high DECIMAL(23,16),
   low DECIMAL(23,16),
